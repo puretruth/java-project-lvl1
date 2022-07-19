ccfc313963551ce4bb31b9444f60a9fc4d28f7d5
puretruth <pure.truth@yandex.ru>
2022-07-19 21:04:11 +0300

    Компиляция и сборка: Настроен gradle для сборки проектаÑ

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, JAVA!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
